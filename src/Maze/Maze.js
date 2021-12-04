c19324feaa982ec594d21d6cac57a4880223b7d4
zhenying <a664330348@gmail.com>
2021-12-03 21:48:30 -0500

    finish mouse onclick

--- src/Maze/Maze.js
+++ src/Maze/Maze.js
@@ -51,87 +51,84 @@ export default function Maze() {
   const [dw] = [80];
 
 
-  const ClearAndDraw = (context,clearX,clearY,drawX,drawY,moveX,moveY,moveNX,moveNY,direction,cases,img1,img2,img3,img4,canvasW, canvasH
-  ) => {
+
+  const ClearAndDrawByMouseCaseB = (context, X, Y, diffX, diffY,moveX,moveY,moveNX,moveNY,img1,img2,img3,img4)=>{
     setStartGameState(false)
     dispatch(handleMove({ px: moveX, py: moveY, nx: moveNX, ny: moveNY }));
     let imgs = [img1, img2, img3, img4];
-    let diff = dw / 4;
-    if (direction ? direction === "right" : false) {
-      for (let i=0; i<4; i++){
-        setTimeout(() => {
-          context.clearRect(clearX * dw + diff*i, clearY * dw, dw, dw);
-          context.drawImage(imgs[i], clearX * dw + diff*(i+1), clearY * dw, dw, dw);
-        }, 100*i);
-      }
-    }else if (direction ? direction === "left" : false) {
-      for (let i=0; i<4; i++){
-        setTimeout(() => {
-          context.clearRect(clearX * dw - diff*i, clearY * dw, dw, dw);
-          context.drawImage(imgs[i], clearX * dw - diff*(i+1), clearY * dw, dw, dw);
-        }, 100*i);
-      }
-    }
-    else {
-      context.clearRect(clearX * dw, clearY * dw, dw, dw);
-      context.drawImage(imgPeople, drawX * dw, drawY * dw, dw, dw);
-      dispatch(handleMove({ px: moveX, py: moveY, nx: moveNX, ny: moveNY }));
-    }
-
-    /* if (cases ==="b"){
-       setTimeout(() => {
-     setStartGameState(true)
-    }, 500); */
-    if(cases ==="e"){
+    
+    for (let i=0; i<4; i++){
       setTimeout(() => {
-        context.drawImage(imgWin,canvasW / 2 - 150,canvasH / 2 - 100,300,200);
-       }, 400);
-    }else{
-      setTimeout(() => {
-        setStartGameState(true)
-       }, 400); 
+        context.clearRect(X * dw + diffX*i, Y * dw + diffY*i, dw, dw);
+        context.drawImage(imgs[i], X * dw + diffX*(i+1), Y * dw + diffY*(i+1), dw, dw);
+      }, 100*i);
     }
    
-  };
+    setTimeout(() => {
+      setStartGameState(true)
+     }, 400); 
+  }
 
+  const ClearAndDrawByMouseCaseE = (context, X, Y, EndX,EndY, diffX, diffY,
+    moveX,moveY,moveNX,moveNY,img1,img2,img3,img4,canvasW,canvasH)=>{
+    setStartGameState(false)
+    dispatch(handleMove({ px: moveX, py: moveY, nx: moveNX, ny: moveNY }));
+    let imgs = [img1, img2, img3, img4];
+    
+    for (let i=0; i<4; i++){
+      setTimeout(() => {
+        context.clearRect(X * dw + diffX*i, Y * dw + diffY*i, dw, dw);
+        context.drawImage(imgs[i], X * dw + diffX*(i+1), Y * dw + diffY*(i+1), dw, dw);
+        context.drawImage(imgEnd, EndX* dw, EndY* dw, dw, dw);
+      }, 100*i);
+    }
+
+    setTimeout(() => {
+      context.clearRect(EndX * dw , EndY * dw , dw, dw);
+      context.drawImage(imgEnd, EndX* dw, EndY* dw, dw, dw);
+      context.drawImage(imgs[3], EndX * dw , EndY * dw, dw, dw);
+      context.drawImage(imgWin,canvasW / 2 - 150,canvasH / 2 - 100,300,200);
+    }, 400);
+  }
 
   const RunGame = (e) => {
     if (StartGameState === true) {
       let canvas = document.getElementById("MazeCanvas");
       let context = canvas.getContext("2d");
       const canvasInfo = canvas.getBoundingClientRect();
-
       let x = Math.floor((e.clientX - canvasInfo.left) / dw);
       let y = Math.floor((e.clientY - canvasInfo.top) / dw);
-
+      
       if (Array[y][x] === "b") {
         if (x - 1 >= 0 ? Array[y][x - 1] === "p" : false) {
           //moveRight
-          ClearAndDraw(context,x - 1,y,x,y,y,x - 1,y,x,"right","b",Right001,Right002,Right003,Right004,
-          );
+          ClearAndDrawByMouseCaseB(context, x - 1, y , dw/4, 0, y,x - 1,y,x,Right001,Right002,Right003,Right004)
         } else if (x + 1 < Array[0].length ? Array[y][x + 1] === "p" : false) {
           //moveLeft
-          ClearAndDraw(context, x + 1, y, x, y, y, x + 1, y, x,"left","b",Left001,Left002,Left003,Left004,);
+          ClearAndDrawByMouseCaseB(context, x + 1, y , -dw/4, 0, y,x + 1,y,x,Left001,Left002,Left003,Left004)
         } else if (y - 1 >= 0 ? Array[y - 1][x] === "p" : false) {
           //moveDown
-          ClearAndDraw(context, x, y - 1, x, y, y - 1, x, y, x);
+          ClearAndDrawByMouseCaseB(context, x, y-1, 0, dw/4, y-1,x,y,x,Right001,Right002,Right003,Right004)
         } else if (y + 1 < Array.length ? Array[y + 1][x] === "p" : false) {
           //moveUp
-          ClearAndDraw(context, x, y + 1, x, y, y + 1, x, y, x);
+          ClearAndDrawByMouseCaseB(context, x, y+1, 0, -dw/4, y+1,x,y,x,Left001,Left002,Left003,Left004)
         }
       } else if (Array[y][x] === "e") {
         if (x - 1 >= 0 ? Array[y][x - 1] === "p" : false) {
-          ClearAndDraw(context, x - 1, y, x, y, y, x - 1, y, x,"right","e",Right001,Right002,Right003,Right004,canvas.width,canvas.height);
-        
+          ClearAndDrawByMouseCaseE(context, x - 1, y ,x,y,dw/4, 0, y,x - 1,y,x,
+            Right001,Right002,Right003,Right004,canvas.width,canvas.height)
+
         } else if (x + 1 < Array[0].length ? Array[y][x + 1] === "p" : false) {
-          ClearAndDraw(context, x + 1, y, x, y, y, x + 1, y, x,"left","e",Left001,Left002,Left003,Left004,canvas.width,canvas.height);
+          ClearAndDrawByMouseCaseE(context, x + 1, y ,x,y, -dw/4, 0, y,x + 1,y,x,
+            Right001,Right002,Right003,Right004,canvas.width,canvas.height)
   
         } else if (y - 1 >= 0 ? Array[y - 1][x] === "p" : false) {
-          ClearAndDraw(context, x, y - 1, x, y, y - 1, x, y, x);
-       
-        } else if (y + 1 < Array.length ? Array[y + 1][x] === "p" : false) {
-          ClearAndDraw(context, x, y + 1, x, y, y + 1, x, y, x);
+          ClearAndDrawByMouseCaseE(context, x, y-1, x,y, 0,dw/4, y-1,x,y,x,
+            Right001,Right002,Right003,Right004,canvas.width,canvas.height)
 
+        } else if (y + 1 < Array.length ? Array[y + 1][x] === "p" : false) {
+          ClearAndDrawByMouseCaseE(context, x, y+1, x,y, 0,-dw/4, y+1,x,y,x,
+            Right001,Right002,Right003,Right004,canvas.width,canvas.height)
         }
       }
     }
@@ -151,14 +148,11 @@ export default function Maze() {
     canvas.height = ArrayInit[difficulty][index].length * dw;
     //context.clearRect(0, 0, canvas.width, canvas.height);
 
-    let px ,py; 
-
     for (let i = 0; i < ArrayInit[difficulty][index].length; i++) {
       for (let j = 0; j < ArrayInit[difficulty][index][0].length; j++) {
         if (ArrayInit[difficulty][index][i][j] === "w") {
           context.drawImage(imgWall, j * dw, i * dw, dw, dw);
         } else if (ArrayInit[difficulty][index][i][j] === "p") {
-          py=i; px=j;
           context.drawImage(imgPeople, j * dw, i * dw, dw, dw);
         } else if (ArrayInit[difficulty][index][i][j] === "e") {
           context.drawImage(imgEnd, j * dw, i * dw, dw, dw);
